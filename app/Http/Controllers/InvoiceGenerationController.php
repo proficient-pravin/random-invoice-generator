120594ed0e0492d8f6863f5af7da8466a2a6ee0d
Pravin Kumar <pkgautam8176@email.com>
2025-01-25 00:18:56 +0530

    fix issue

--- app/Http/Controllers/InvoiceGenerationController.php
+++ app/Http/Controllers/InvoiceGenerationController.php
@@ -12,6 +12,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Log;
 use ZipArchive;
 
 class InvoiceGenerationController extends Controller
