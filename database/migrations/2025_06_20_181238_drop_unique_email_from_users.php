854dc6ba2231b5d5d1c05dc80c713650104447b5
Pravin Kumar <pkgautam8176@email.com>
2025-06-21 00:19:50 +0530

    drop unique email

--- database/migrations/2025_06_20_181238_drop_unique_email_from_users.php
+++ database/migrations/2025_06_20_181238_drop_unique_email_from_users.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('email')->unique()->nullable()->change();
+            $table->string('email')->nullable()->change();
         });
     }
 
@@ -22,7 +22,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('email')->unique()->nullable()->change();
+            $table->string('email')->nullable()->change();
         });
     }
 };
