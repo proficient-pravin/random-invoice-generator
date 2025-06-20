be3604ae138007dd831d526a241b9e9afa9e1f77
Pravin Kumar <pkgautam8176@email.com>
2025-06-21 01:36:17 +0530

    Added log

--- app/Http/Controllers/Invoice/InvoiceController.php
+++ app/Http/Controllers/Invoice/InvoiceController.php
@@ -25,12 +25,12 @@ class InvoiceController extends Controller
                         });
                     })
                     ->when(request()->start_date && request()->end_date, function ($q) {
-                        $q->whereBetween('invoice_date', [request()->start_date, request()->end_date]);
+                        $q->whereBetween('invoices.invoice_date', [request()->start_date, request()->end_date]);
                     })
-                    // ->select('invoices.id', 'invoices.invoice_number', 'invoices.invoice_date', 'invoices.customer_id') // Select the necessary columns
-                    // ->addSelect(\DB::raw('ROUND(SUM(invoice_items.amount + invoice_items.tax), 2) as total'))           // Calculate the total dynamically from invoice items
-                    // ->addSelect(\DB::raw('ROUND(SUM(invoice_items.amount), 2) as sub_total'))                           // Calculate the total dynamically from invoice items
-                    // ->addSelect(\DB::raw('ROUND(SUM(invoice_items.tax), 2) as tax'))                                    // Calculate the total dynamically from invoice items
+                    ->select('invoices.id', 'invoices.invoice_number', 'invoices.invoice_date', 'invoices.customer_id') // Select the necessary columns
+                    ->addSelect(\DB::raw('ROUND(SUM(invoice_items.amount + invoice_items.tax), 2) as total'))           // Calculate the total dynamically from invoice items
+                    ->addSelect(\DB::raw('ROUND(SUM(invoice_items.amount), 2) as sub_total'))                           // Calculate the total dynamically from invoice items
+                    ->addSelect(\DB::raw('ROUND(SUM(invoice_items.tax), 2) as tax'))                                    // Calculate the total dynamically from invoice items
                     ->join('invoice_items', 'invoice_items.invoice_id', '=', 'invoices.id')                             // Join invoice_items table to get the amounts
                     ->groupBy('invoices.id')
                     ->get();
