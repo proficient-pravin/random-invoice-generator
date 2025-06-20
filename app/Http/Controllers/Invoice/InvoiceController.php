180ff68fa1fd9f3295f3cf923f5f34d54a36c128
Pravin Kumar <pkgautam8176@email.com>
2025-06-21 01:05:25 +0530

    Added log

--- app/Http/Controllers/Invoice/InvoiceController.php
+++ app/Http/Controllers/Invoice/InvoiceController.php
@@ -6,6 +6,7 @@ use App\Models\Customer;
 use App\Models\Invoice;
 use Barryvdh\DomPDF\Facade\Pdf as PDF;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 use Yajra\DataTables\Facades\DataTables;
 
 class InvoiceController extends Controller
@@ -16,44 +17,49 @@ class InvoiceController extends Controller
     public function index()
     {
         if (request()->ajax()) {
-            $invoices = Invoice::with('customer', 'items')
-                ->when(! empty(request()->customer), function ($q) {
-                    $q->whereHas('customer', function ($q) {
-                        $q->whereIn('id', request()->customer);
-                    });
-                })
-                ->when(request()->start_date && request()->end_date, function ($q) {
-                    $q->whereBetween('invoice_date', [request()->start_date, request()->end_date]);
-                })
-                ->select('invoices.id', 'invoices.invoice_number', 'invoices.invoice_date', 'invoices.customer_id') // Select the necessary columns
-                ->addSelect(\DB::raw('ROUND(SUM(invoice_items.amount + invoice_items.tax), 2) as total'))           // Calculate the total dynamically from invoice items
-                ->addSelect(\DB::raw('ROUND(SUM(invoice_items.amount), 2) as sub_total'))                           // Calculate the total dynamically from invoice items
-                ->addSelect(\DB::raw('ROUND(SUM(invoice_items.tax), 2) as tax'))                                    // Calculate the total dynamically from invoice items
-                ->join('invoice_items', 'invoice_items.invoice_id', '=', 'invoices.id')                             // Join invoice_items table to get the amounts
-                ->groupBy('invoices.id')
-                ->get();
+            try{
 
-            return DataTables::of($invoices)
-                ->addColumn('customer_name', function ($invoice) {
-                    return $invoice->customer->first_name . " " . $invoice->customer->last_name; // Return the full name of the customer
-                })
-                ->addColumn('invoice_items', function ($invoice) {
-                    return $invoice->items->map(function ($item) {
-                        return [
-                            'name'           => $item->name,
-                            'description'    => $item->description,
-                            'quantity'       => $item->quantity,
-                            'unit_price'     => $item->unit_price,
-                            'tax'            => $item->tax,
-                            'tax_percentage' => $item->tax_percentage,
-                            'amount'         => $item->amount,
-                        ];
-                    });
-                })
-                ->addColumn('actions', function ($customer) {
-                    return view('invoices.actions', compact('customer'));
-                })
-                ->make(true);
+                $invoices = Invoice::with('customer', 'items')
+                    ->when(! empty(request()->customer), function ($q) {
+                        $q->whereHas('customer', function ($q) {
+                            $q->whereIn('id', request()->customer);
+                        });
+                    })
+                    ->when(request()->start_date && request()->end_date, function ($q) {
+                        $q->whereBetween('invoice_date', [request()->start_date, request()->end_date]);
+                    })
+                    ->select('invoices.id', 'invoices.invoice_number', 'invoices.invoice_date', 'invoices.customer_id') // Select the necessary columns
+                    ->addSelect(\DB::raw('ROUND(SUM(invoice_items.amount + invoice_items.tax), 2) as total'))           // Calculate the total dynamically from invoice items
+                    ->addSelect(\DB::raw('ROUND(SUM(invoice_items.amount), 2) as sub_total'))                           // Calculate the total dynamically from invoice items
+                    ->addSelect(\DB::raw('ROUND(SUM(invoice_items.tax), 2) as tax'))                                    // Calculate the total dynamically from invoice items
+                    ->join('invoice_items', 'invoice_items.invoice_id', '=', 'invoices.id')                             // Join invoice_items table to get the amounts
+                    ->groupBy('invoices.id')
+                    ->get();
+    
+                return DataTables::of($invoices)
+                    ->addColumn('customer_name', function ($invoice) {
+                        return $invoice->customer->first_name . " " . $invoice->customer->last_name; // Return the full name of the customer
+                    })
+                    ->addColumn('invoice_items', function ($invoice) {
+                        return $invoice->items->map(function ($item) {
+                            return [
+                                'name'           => $item->name,
+                                'description'    => $item->description,
+                                'quantity'       => $item->quantity,
+                                'unit_price'     => $item->unit_price,
+                                'tax'            => $item->tax,
+                                'tax_percentage' => $item->tax_percentage,
+                                'amount'         => $item->amount,
+                            ];
+                        });
+                    })
+                    ->addColumn('actions', function ($customer) {
+                        return view('invoices.actions', compact('customer'));
+                    })
+                    ->make(true);
+            }catch(\Exception $e){
+                Log::error($e->getMessage());
+            }
         }
 
         return view('invoices.index', [
